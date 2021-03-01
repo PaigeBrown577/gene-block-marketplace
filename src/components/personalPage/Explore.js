9075efcb37e91c53f298cd4549d3e29d56c31644
Colbert Xuan <xuancc@g.ucla.edu>
2021-03-01 03:59:52 -0800

    added search bar to explore page

--- src/components/personalPage/Explore.js
+++ src/components/personalPage/Explore.js
@@ -1,10 +1,18 @@
 import React from "react";
 import "./Explore.css";
 
+import SearchIcon from "@material-ui/icons/Search";
+
 function Explore() {
   return (
       <div className="explore">
         <h1>Explore</h1>
+
+        <div className="searchBar">
+            <SearchIcon className="widgets__searchIcon" />
+            <input placeholder="Search" type="text" />
+        </div>
+
       </div>
   );
 }
