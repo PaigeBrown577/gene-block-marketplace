9726dd5e4aedbe98160a7dcc3698ffde595e43f9
Colbert Xuan <xuancc@g.ucla.edu>
2021-03-05 01:18:03 -0800

    changed the makePost form so it now uses controlled components

--- client/src/components/personalPage/MakePost.js
+++ client/src/components/personalPage/MakePost.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "../../styles/MakePost.css";
 
 import Form from "react-bootstrap/Form";
@@ -7,6 +7,68 @@ import Button from "react-bootstrap/Button";
 import DropdownMultiselect from "react-multiselect-dropdown-bootstrap";
 
 function MakePost() {
+    const [title, setTitle] = useState("");
+    const [price, setPrice] = useState("");
+    const [description, setDescription] = useState("");
+    const [dropdownMeetingLocation, setDropdownMeetingLocation] = useState("Powell");
+    const [otherMeetingLocation, setOtherMeetingLocation] = useState("");
+    const [tag, setTag] = useState("Books");
+
+    const [finalMeetingLocation, setFinalMeetingLocation] = useState("");
+    // this is the actual one that gets submitted to the database, after considering the user's
+    // input into the dropdown menu and the other location textbox
+
+    
+
+    const handleTitleChange = (event) => {
+        setTitle(event.target.value);
+    }
+
+    const handlePriceChange = (event) => {
+        setPrice(event.target.value);
+    }
+
+    const handleDescriptionChange = (event) => {
+        setDescription(event.target.value);
+    }
+
+    const handleDropdownLocationChange = (event) => {
+        setDropdownMeetingLocation(event.target.value);
+        setFinalMeetingLocation(event.target.value);
+
+        if (event.target.value === "Other") {
+            setFinalMeetingLocation(otherMeetingLocation);
+        }
+    }
+
+    const handleOtherLocationChange = (event) => {
+        setOtherMeetingLocation(event.target.value);
+
+        // only change the location if the "other" dropdown option has been selected
+        if (dropdownMeetingLocation === "Other") {
+            setFinalMeetingLocation(event.target.value);
+        }
+    }
+
+    const handleTagChange = (event) => {
+        setTag(event.target.value);
+    }
+
+    // don't know how to deal with uploaded images yet
+
+
+
+
+
+
+    // console.log(title);
+    // console.log(price);
+    // console.log(description);
+    // console.log(dropdownMeetingLocation);
+    // console.log(otherMeetingLocation);
+    // console.log(finalMeetingLocation);
+    // console.log(tag);
+
 
   return (
       <div className="makepost">
@@ -19,25 +81,25 @@ function MakePost() {
 
 
         <div className="makepostForm">
-            <Form>
+            <Form >
                 <Form.Group controlId="exampleForm.ControlInput1">
                     <Form.Label>Title</Form.Label>
-                    <Form.Control type="text" placeholder="Title" />
+                    <Form.Control type="text" placeholder="Title" value={title} onChange={handleTitleChange} />
                 </Form.Group>
                 <Form.Group controlId="exampleForm.ControlInput1">
                     <Form.Label>Price ($)</Form.Label>
-                    <Form.Control type="text" placeholder="Price" />
+                    <Form.Control type="text" placeholder="Price" value={price} onChange={handlePriceChange} />
                 </Form.Group>
                 <Form.Group controlId="exampleForm.ControlTextarea1">
                     <Form.Label>Description</Form.Label>
-                    <Form.Control as="textarea" rows={5} />
+                    <Form.Control as="textarea" rows={5} value={description} onChange={handleDescriptionChange} />
                 </Form.Group>
                 <Form.Group>
                     <Form.File id="exampleFormControlFile1" label="(optional) Upload images" />
                 </Form.Group>
                 <Form.Group controlId="exampleForm.ControlSelect1">
                     <Form.Label>Meeting Location</Form.Label>
-                    <Form.Control as="select">
+                    <Form.Control as="select" value={dropdownMeetingLocation} onChange={handleDropdownLocationChange} >
                     <option>Powell</option>
                     <option>Boelter Hall</option>
                     <option>Royce</option>
@@ -46,11 +108,11 @@ function MakePost() {
                 </Form.Group>
                 <Form.Group controlId="exampleForm.ControlInput1">
                     <Form.Label>(optional) If selected other meeting location, please specify:</Form.Label>
-                    <Form.Control type="text" placeholder="Other location" />
+                    <Form.Control type="text" placeholder="Other location" value={otherMeetingLocation} onChange={handleOtherLocationChange} />
                 </Form.Group>
                 <Form.Group controlId="exampleForm.ControlSelect1">
                     <Form.Label>Tag</Form.Label>
-                    <Form.Control as="select">
+                    <Form.Control as="select" value={tag} onChange={handleTagChange} >
                     <option>Books</option>
                     <option>Furniture</option>
                     <option>Swipes</option>
@@ -68,8 +130,6 @@ function MakePost() {
                 <Button variant="primary" type="submit">Post</Button>
             </Form>
         </div>
-
-
 
         
       </div>
