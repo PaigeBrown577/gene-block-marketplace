3df720f60fdaa3c2c6bfa19a3b5dcfd52d6f1e1a
Colbert Xuan <xuancc@g.ucla.edu>
2021-03-09 15:01:08 -0800

    not too sure

--- client/src/components/personalPage/Messages.js
+++ client/src/components/personalPage/Messages.js
@@ -18,6 +18,9 @@ import {
   Link
 } from "react-router-dom";
 
+// look in sendNewMessage for fields
+// hard code some stuff for now
+
 function Messages({userID, setUserID}) {
   const [userEmail, setUserEmail] = useState("");
 
